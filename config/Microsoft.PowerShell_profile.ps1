05b0c785c3b078db59af6829a92f98e9bb0b922b
Valentin <valentin.mariette@cloud-temple.lan>
2017-11-24 17:42:04 +0100

    commit #3
    Edit profile

--- config/Microsoft.PowerShell_profile.ps1
+++ config/Microsoft.PowerShell_profile.ps1
@@ -6,7 +6,6 @@ Write-host "[INFO] - liste des modules disponible : "
 Get-module -ListAvailable | select Name,Version |ft 
 Write-host "[INFO] - Pour importer : Import-module moduleName"
 write-host "[INFO] - Pour charger tous les modules : "
-write-host '[INFO] - get-module -ListAvailable | ForEach-Object { write-host "[INFO] - Import $_.Name " ; Import-module $_.Name } '
 $validation = read-host '[INFO] - Chargement de tous les modules ? [Y/N]'
 if ($validation -eq  'Y')
 {
